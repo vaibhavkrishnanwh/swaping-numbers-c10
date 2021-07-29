1b4c429feb102d35cf109a4be6541cd2fdd6dd4b
vaibhavkrishnanwh <88144325+vaibhavkrishnanwh@users.noreply.github.com>
2021-07-29 17:40:30 +0530

    swap

---
+++ sketch.js
@@ -0,0 +1,19 @@
+
+
+var a = prompt('Enter the first variable: ');
+var b = prompt('Enter the second variable: ');
+function setup(){
+var b2 = createButton("Click here to swap the numbers.");
+  b2.mousePressed(swap);
+  }
+
+  
+function draw()
+{
+}
+function swap(){
+[a, b] = [b, a];
+
+console.log("The value of a after swapping:" ,+a);
+console.log("The value of b after swapping:",+b);
+}
